9e1702b7713d55d779e5812ef8cf3a90aff274b3
Ferdinando Ametrano <nando@ametrano.net>
2015-04-22 12:21:06 +0200

    fixed wrong header file names

--- ql/termstructures/yield/tenorbasis.cpp
+++ ql/termstructures/yield/tenorbasis.cpp
@@ -17,7 +17,7 @@
  FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
 
-#include <ql/termstructures/yield/integratedcontinuousbasis.hpp>
+#include <ql/termstructures/yield/tenorbasis.hpp>
 #include <ql/indexes/iborindex.hpp>
 
 using boost::shared_ptr;
