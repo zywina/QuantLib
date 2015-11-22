4017135d040d340a866d2a085d9d51cf7f72044c
paolomazzocchi <mazzocchip@live.it>
2015-11-22 14:53:44 +0100

    working files

---
+++ ql/experimental/tenorbasis/forwardratecurve.cpp
@@ -0,0 +1,91 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2015 Ferdinando Ametrano
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+#include <ql/experimental/tenorbasis/forwardratecurve.hpp>
+
+namespace QuantLib {
+
+    ForwardRateCurve::ForwardRateCurve(const std::string& fwdFamilyName,
+                                       const Period& fwdTenor,
+                                       Natural fwdSettlementDays,
+                                       const Currency& fwdCurrency,
+                                       const Calendar& fwdFixingCalendar,
+                                       BusinessDayConvention fwdConvention,
+                                       bool fwdEndOfMonth,
+                                       const DayCounter& fwdDayCounter,
+                                       const DayCounter& dc)
+    : TermStructure(dc),
+      fwdFamilyName_(fwdFamilyName), fwdTenor_(fwdTenor),
+      fwdSettlementDays_(fwdSettlementDays), fwdCurrency_(fwdCurrency),
+      fwdFixingCalendar_(fwdFixingCalendar), fwdConvention_(fwdConvention),
+      fwdEndOfMonth_(fwdEndOfMonth), fwdDayCounter_(fwdDayCounter) {}
+
+    ForwardRateCurve::ForwardRateCurve(const std::string& fwdFamilyName,
+                                       const Period& fwdTenor,
+                                       Natural fwdSettlementDays,
+                                       const Currency& fwdCurrency,
+                                       const Calendar& fwdFixingCalendar,
+                                       BusinessDayConvention fwdConvention,
+                                       bool fwdEndOfMonth,
+                                       const DayCounter& fwdDayCounter,
+                                       const Date& referenceDate,
+                                       const Calendar& cal,
+                                       const DayCounter& dc)
+    : TermStructure(referenceDate, cal, dc),
+      fwdFamilyName_(fwdFamilyName), fwdTenor_(fwdTenor),
+      fwdSettlementDays_(fwdSettlementDays), fwdCurrency_(fwdCurrency),
+      fwdFixingCalendar_(fwdFixingCalendar), fwdConvention_(fwdConvention),
+      fwdEndOfMonth_(fwdEndOfMonth), fwdDayCounter_(fwdDayCounter) {}
+
+    ForwardRateCurve::ForwardRateCurve(const std::string& fwdFamilyName,
+                                       const Period& fwdTenor,
+                                       Natural fwdSettlementDays,
+                                       const Currency& fwdCurrency,
+                                       const Calendar& fwdFixingCalendar,
+                                       BusinessDayConvention fwdConvention,
+                                       bool fwdEndOfMonth,
+                                       const DayCounter& fwdDayCounter,
+                                       Natural settlementDays,
+                                       const Calendar& cal,
+                                       const DayCounter& dc)
+    : TermStructure(settlementDays, cal, dc),
+      fwdFamilyName_(fwdFamilyName), fwdTenor_(fwdTenor),
+      fwdSettlementDays_(fwdSettlementDays), fwdCurrency_(fwdCurrency),
+      fwdFixingCalendar_(fwdFixingCalendar), fwdConvention_(fwdConvention),
+      fwdEndOfMonth_(fwdEndOfMonth), fwdDayCounter_(fwdDayCounter) {}
+
+    InterestRate ForwardRateCurve::forwardInterestRate(const Date& d,
+                                                       bool extrap) const {
+        Time t = timeFromReference(d);
+        return forwardInterestRate(t, extrap);
+    }
+
+    Rate ForwardRateCurve::forwardRate(const Date& d,
+                                       bool extrap) const {
+        Time t = timeFromReference(d);
+        return forwardRate(t, extrap);
+    }
+
+    InterestRate ForwardRateCurve::forwardInterestRate(Time t,
+                                                       bool extrap) const {
+        Rate fwd = forwardRate(t, extrap);
+        return InterestRate(fwd, fwdDayCounter_, Simple, Annual);
+    }
+
+}
---
+++ ql/experimental/tenorbasis/forwardratecurve.hpp
@@ -0,0 +1,111 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2015 Ferdinando Ametrano
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file forwardratecurve.hpp
+    \brief Interest-rate forward rate curve
+*/
+
+#ifndef quantlib_forward_rate_curve_hpp
+#define quantlib_forward_rate_curve_hpp
+
+#include <ql/termstructure.hpp>
+#include <ql/currency.hpp>
+#include <ql/interestrate.hpp>
+
+namespace QuantLib {
+
+    //! Term structure able to calculate forward interest rates (no discounts)
+    /*! blah blah
+    */
+    class ForwardRateCurve : public TermStructure {
+      public:
+        /*! \name Constructors
+            See the TermStructure documentation for issues regarding
+            constructors.
+        */
+        //@{
+        ForwardRateCurve(const std::string& fwdFamilyName,
+                         const Period& fwdTenor,
+                         Natural fwdSettlementDays,
+                         const Currency& fwdCurrency,
+                         const Calendar& fwdFixingCalendar,
+                         BusinessDayConvention fwdConvention,
+                         bool fwdEndOfMonth,
+                         const DayCounter& fwdDayCounter,
+                         const DayCounter& dc = DayCounter());
+        ForwardRateCurve(const std::string& fwdFamilyName,
+                         const Period& fwdTenor,
+                         Natural fwdSettlementDays,
+                         const Currency& fwdCurrency,
+                         const Calendar& fwdFixingCalendar,
+                         BusinessDayConvention fwdConvention,
+                         bool fwdEndOfMonth,
+                         const DayCounter& fwdDayCounter,
+                         const Date& referenceDate,
+                         const Calendar& cal = Calendar(),
+                         const DayCounter& dc = DayCounter());
+        ForwardRateCurve(const std::string& fwdFamilyName,
+                         const Period& fwdTenor,
+                         Natural fwdSettlementDays,
+                         const Currency& fwdCurrency,
+                         const Calendar& fwdFixingCalendar,
+                         BusinessDayConvention fwdConvention,
+                         bool fwdEndOfMonth,
+                         const DayCounter& fwdDayCounter,
+                         Natural settlementDays,
+                         const Calendar& cal,
+                         const DayCounter& dc = DayCounter());
+        //@}
+        /*! \name Forward rates
+
+            These methods returns the forward interest rate for the fixed
+            given tenor at a given date or time.  In the former case, time
+            is calculated as fractions of year from the reference date,
+            using the term structure daycounter.
+        */
+        //@{
+        /*! The resulting interest rate has the required day-counting rule.
+        */
+        InterestRate forwardInterestRate(const Date& d,
+                                         bool extrapolate = false) const;
+        Rate forwardRate(const Date& d,
+                         bool extrapolate = false) const;
+        /*! The resulting interest rate has the required day-counting rule.
+            The term structure daycounter has to be used for calculating the
+            passed time t.
+        */
+        InterestRate forwardInterestRate(Time t,
+                                         bool extrapolate = false) const;
+        virtual Rate forwardRate(Time t,
+                                 bool extrapolate = false) const = 0;
+        //@}
+      protected:
+        std::string fwdFamilyName_;
+        Period fwdTenor_;
+        Natural fwdSettlementDays_;
+        Currency fwdCurrency_;
+        Calendar fwdFixingCalendar_;
+        BusinessDayConvention fwdConvention_;
+        bool fwdEndOfMonth_;
+        DayCounter fwdDayCounter_;
+    };
+
+}
+
+#endif
