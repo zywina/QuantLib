341f1bcb67f8bc8140437b15573655c80be46508
Roy Zywina <roy@zywina.ca>
2018-03-17 11:36:23 -0400

    Powell's method optimization

---
+++ ql/math/optimization/powell.hpp
@@ -0,0 +1,358 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2018 Roy Zywina
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file powell.hpp
+    \brief Powell's optimization method
+*/
+
+#ifndef quantlib_optimization_powell_hpp
+#define quantlib_optimization_powell_hpp
+
+#include <stdio.h>
+#include <ql/math/comparison.hpp>
+#include <ql/utilities/null.hpp>
+#include <ql/patterns/curiouslyrecurring.hpp>
+#include <ql/errors.hpp>
+#include <ql/math/optimization/linesearchbasedmethod.hpp>
+#include <ql/math/optimization/linesearch.hpp>
+#include <ql/math/optimization/problem.hpp>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+namespace QuantLib{
+
+  template <class Impl>
+  class Minimizer1D : public CuriouslyRecurringTemplate<Impl> {
+  public:
+    Minimizer1D(){}
+
+    template <class F>
+    Real minimize(const F& f,
+               Real accuracy,
+               Real guess,
+               Real step)
+    {
+      xMin_=guess;
+      xMax_=guess+step;
+      bracket(f,xMin_,xMax_,xMid_,fxMin_,fxMax_,fxMid_);
+      return this->impl().minimizeImpl(f, accuracy);
+    }
+
+    template <class F>
+    void bracket(const F& f,
+      Real &ax,Real &bx,Real &cx,
+      Real &fa,Real &fb,Real &fc)
+    {
+      fa = f(ax);
+      fb = f(bx);
+      if (fb > fa){
+        std::swap(fa,fb);
+        std::swap(ax,bx);
+      }
+      const Real growth = 1.6;
+      cx = bx + growth*(bx-ax);
+      fc = f(cx);
+      while (fb > fc){
+        // TODO: implement parabolic optimization
+        Real u = cx + growth*(cx-bx);
+        Real fu = f(u);
+        // remove oldest point
+        ax=bx; bx=cx; cx=u;
+        fa=fb; fb=fc; fc=fu;
+      }
+      if (ax > bx){
+        std::swap(fa,fb);
+        std::swap(ax,bx);
+      }
+    }
+  protected:
+    Real xMin_,xMax_,xMid_,fxMin_,fxMax_,fxMid_;
+  };
+
+  class GoldenSection : public Minimizer1D<GoldenSection>{
+  public:
+    template <class F>
+    Real minimizeImpl(const F& f, Real xAccuracy) {
+      const Real gold = (sqrt(5.0)+1)/2;
+      Real xc,xd,yc,yd;
+      do{
+        xc = xMax_ - (xMax_ - xMin_) / gold;
+        xd = xMin_ + (xMax_ - xMin_) / gold;
+        yc = f(xc);
+        yd = f(xd);
+        if (yc < yd){
+          xMax_ = xd;
+          fxMax_ = yd;
+          xMid_ = xc;
+          fxMid_ = yc;
+        }else{
+          xMin_ = xc;
+          fxMin_ = yc;
+          xMid_ = xd;
+          fxMid_ = yd;
+        }
+        //printf("%.8f\n",std::abs(xc-xd));
+      }while (std::abs(xc-xd)>xAccuracy);
+      return (xMin_+xMax_)/2;
+    }
+  };
+
+  class BrentMinimizer : public Minimizer1D<BrentMinimizer>{
+  public:
+    template <class F>
+    Real minimizeImpl(const F& f, Real xAccuracy) {
+      Real a,b,d,x,w,v,fx,fv,fw,xm,p,q,r,u,fu;
+      Real tol1,tol2,tol=0.001,e=0,etemp;
+      a = xMin_;
+      b = xMax_;
+      x=w=v= xMid_;
+      fw=fv=fx = fxMid_;
+      const Real CGOLD = 0.3819660;
+      for (int iter=0; iter<100; iter++){
+        xm = (a+b)/2;
+        tol1 = tol*std::abs(x)+QL_EPSILON;
+        tol2 = 2*tol1;
+        if (std::abs(x-xm) <= (tol2-0.5*(b-a))){
+          return x;
+        }
+        if (std::abs(e) > tol1){
+          r = (x-w)*(fx-fv);
+          q = (x-v)*(fx-fw);
+          p = (x-v)*q-(x-w)*r;
+          q=2.0*(q-r);
+          if (q>0.0) p = -p;
+          q = std::abs(q);
+          etemp=e;
+          e=d;
+          if (std::abs(p) >= std::abs(0.5*q*etemp) ||
+            p <= q*(a-x) || p >= q*(b-x))
+          {
+            e = x >= xm ? a-x : b-x;
+            d = CGOLD * e;
+          }else{
+            d = p/q;
+            u = x+d;
+            if (u-a < tol2 || b-u < tol2)
+              d = sign(tol1,xm-x);
+          }
+        }else{
+          e = x >= xm ? a-x : b-x;
+          d = CGOLD * e;
+        }
+        u = std::abs(d) >= tol1 ? x+d : x+sign(tol1,d);
+        fu = f(u);
+        //printf("Brent f(%f) = %f\n",u,fu);
+        if (fu <= fx){
+          if (u>=x)
+            a=x;
+          else
+            b=x;
+          v=w; w=x; x=u;
+          fv=fw; fw=fx; fx=fu;
+        }else{
+          if (u<x) a=u; else b=u;
+          if (fu <= fw || w==x){
+            v=w;
+            w=u;
+            fv=fw;
+            fw=fu;
+          }else if (fu<=fv || v==x || v==w){
+            v=u;
+            fv=fu;
+          }
+        }
+      }
+      return x;
+    }
+    Real sign(Real a, Real b) const {
+        return b >= 0.0 ? std::fabs(a) : -std::fabs(a);
+    }
+  };
+
+  //template <class Minimizer = BrentMinimizer>
+  class Powell : public OptimizationMethod {
+    public:
+      enum DirectionSet{
+        None,
+        Unit,
+        Cosine,
+        Walsh
+      };
+      Powell(DirectionSet directionSet = Powell::Unit) :
+        OptimizationMethod(),
+        directionset_(directionSet)
+        {}
+      Powell(const std::vector<Array> &directions) :
+        OptimizationMethod(),
+        directionset_(Powell::None),
+        direction_(directions)
+        {}
+
+      virtual EndCriteria::Type minimize(Problem& P,
+            const EndCriteria& endCriteria)
+      {
+        EndCriteria::Type ecType = EndCriteria::None;
+        P.reset();
+        current_ = P.currentValue();
+        Size iterationNumber_ = 0;
+        Size N = current_.size();
+        if (directionset_ != Powell::None){
+          rebuildDirections(N);
+        }
+        Real xtol = endCriteria.rootEpsilon();
+        boost::function<Real(Real)> f =
+          boost::bind(&Powell::calculate,this,boost::ref(P),_1);
+        for (int i=0; i<20; i++){
+          Array avg(N,0);
+          for (size_t j=0; j<N; j++){
+            current_ = P.currentValue();
+            dir_ = direction_[j];
+            Real x = BrentMinimizer().minimize(
+              f,xtol, 0.0, 0.01);
+            dir_ *= x;
+            avg += dir_;
+          }
+          current_ = P.currentValue();
+          dir_ = avg;
+          // search the average movement vector
+          BrentMinimizer().minimize(
+            f,xtol, 0.0, 0.01);
+        }
+
+        return ecType;
+      }
+    private:
+      // direction vectors
+      DirectionSet directionset_;
+      std::vector<Array> direction_;
+      boost::shared_ptr<LineSearch> lineSearch_;
+      Array current_, dir_;
+
+      void rebuildDirections(Size N){
+        direction_.resize(N);
+        for (Size i=0; i<direction_.size(); i++){
+          direction_[i] = Array(N,0.0);
+          if (directionset_ == Powell::Unit){
+            direction_[i][i] = 1.0;
+          }else if (directionset_ == Powell::Cosine){
+            for (Size j=0; j<N; j++){
+              direction_[i][j] = cos((i)*M_PI*j/(Real)(N-1));
+            }
+          }else if (directionset_ == Powell::Walsh){
+            Size k=0;
+            for (Size j=0; j<N; j++){
+              k += i * j;
+              direction_[i][j] = k%2==0 ? 1 : -1;
+            }
+          }
+          std::cout << i <<": " <<direction_[i]<<std::endl;
+        }
+      }
+
+      Real calculate(Problem &P, Real x){
+        Array xx = dir_;
+        xx *= x;
+        xx += current_;
+        Real err = P.value(xx);
+        if (err < P.functionValue()){
+          P.setFunctionValue(err);
+          P.setCurrentValue(xx);
+          //for (Size i=0; i<xx.size(); i++)
+          //  printf("%g ",xx[i]);
+          //printf(" =  %.8g\n",err);
+        }
+        return err;
+      }
+  };
+
+  /*
+  Powell::Powell() :
+    OptimizationMethod()
+  {
+  }
+
+  EndCriteria::Type Powell::minimize(Problem& P, const EndCriteria& endCriteria)
+  {
+    EndCriteria::Type ecType = EndCriteria::None;
+    P.reset();
+    current_ = P.currentValue();
+    Size iterationNumber_ = 0;
+    Size N = current_.size();
+    if (direction_.size()!=N)
+      rebuildDirections(N);
+
+    boost::function<Real(Real)> f =
+      boost::bind(&Powell::calculate,this,&P,_1);
+    for (int i=0; i<2; i++){
+      Array avg(N,0);
+      for (size_t j=0; j<N; j++){
+        current_ = P.currentValue();
+        dir_ = direction_[j];
+        Real x = BrentMinimizer().minimize(
+          boost::bind(&Powell::calculate,this,&P,_1),
+          1e-6, 0.0, 0.01);
+        dir_ *= x;
+        avg += dir_;
+      }
+      current_ = P.currentValue();
+      dir_ = avg;
+      // search the average movement vector
+      BrentMinimizer().minimize(
+        boost::bind(&Powell::calculate,this,&P,_1),
+        1e-6, 0.0, 0.01);
+    }
+
+    return ecType;
+  }
+
+  Real Powell::calculate(Problem *P, Real x){
+    Array xx = dir_;
+    xx *= x;
+    xx += current_;
+    //for (size_t i=0; i<current_.size(); i++){
+    //  xx[i] = current_[i] + dir_[i]*x;
+    //}
+    Real err = P->value(xx);
+    if (err < P->functionValue()){
+      P->setFunctionValue(err);
+      P->setCurrentValue(xx);
+      for (Size i=0; i<xx.size(); i++)
+        printf("%f ",xx[i]);
+      printf(" =  %.8f\n",err);
+    }
+    return err;
+  }
+
+  void Powell::rebuildDirections(Size N){
+    // default to unit direction vectors
+    direction_.resize(N);
+    for (Size i=0; i<direction_.size(); i++){
+      direction_[i] = Array(N,0.0);
+      direction_[i][i] = 1.0;
+
+      for (Size j=0; j<N; j++){
+        direction_[i][j] = cos(i*M_PI*j/(Real)(N-1));
+      }
+    }
+  }
+  */
+
+};
+
+#endif
